4e90c27872f558eacdc2a9884dbc57b8208af353
nezd53 <nezdatsdev@gmail.com>
2020-12-23 18:13:22 -0800

    Add nausea effect

--- src/main/java/nezd53/sneakfart/FartHandler.java
+++ src/main/java/nezd53/sneakfart/FartHandler.java
@@ -5,7 +5,11 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
+
+import java.util.List;
 
 import static java.lang.Math.*;
 import static nezd53.sneakfart.SneakFart.*;
@@ -23,21 +27,34 @@ public class FartHandler {
     }
 
     static void fart(Player player) {
-        Particle.DustOptions options = new Particle.DustOptions(Color.fromRGB(139, 69, 19), (float)fartParticleSize);
+        Particle.DustOptions options = new Particle.DustOptions(Color.fromRGB(139, 69, 19), (float) fartParticleSize);
         float yaw = player.getLocation().getYaw();
         Vector offset = new Vector(sin(toRadians(yaw)) * fartDistance, 0.25, -cos(toRadians(yaw)) * fartDistance);
         Location l = player.getLocation().add(offset);
         player.spawnParticle(Particle.REDSTONE, l,
                 25, fartOffset, fartOffset, fartOffset, options);
-        player.playSound(l, Sound.BLOCK_WET_GRASS_PLACE, (float)fartVolume, 0.005f);
+        player.playSound(l, Sound.BLOCK_WET_GRASS_PLACE, (float) fartVolume, 0.005f);
 
-        if (random() <= poopChance) {
-                Item item = player.getWorld().dropItem(l, new ItemStack(Material.BROWN_DYE));
-                item.setPickupDelay(32767);
-                item.setTicksLived(5900);
-                item.setCustomName(player.getName() + "'s Poop");
-                item.setCustomNameVisible(true);
-                item.setVelocity(offset.divide(new Vector(10,1,10)));
-        }
+        if (random() < poopChance)
+            poop(player, offset, l);
+
+        if (random() < nauseaChance)
+            inflictNausea(l);
+    }
+
+    private static void poop(Player player, Vector offset, Location l) {
+        Item item = player.getWorld().dropItem(l, new ItemStack(Material.BROWN_DYE));
+        item.setPickupDelay(32767);
+        item.setTicksLived(5900);
+        item.setCustomName(player.getName() + "'s Poop");
+        item.setCustomNameVisible(true);
+        item.setVelocity(offset.clone().divide(new Vector(10, 1, 10)));
+    }
+
+    private static void inflictNausea(Location l) {
+        List<Player> players = l.getWorld().getPlayers();
+        for (Player p : players)
+            if (p.getLocation().distance(l) <= nauseaDistance)
+                p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 5 * 20, 5));
     }
 }
--- src/main/java/nezd53/sneakfart/SneakFart.java
+++ src/main/java/nezd53/sneakfart/SneakFart.java
@@ -6,23 +6,25 @@ public final class SneakFart extends JavaPlugin {
 
     static boolean enableFarts;
     static double fartDistance, fartTimeStart, fartTimeEnd,
-            fartOffset, fartParticleSize, fartVolume, poopChance;
+            fartOffset, fartParticleSize, fartVolume, poopChance, nauseaChance, nauseaDistance;
     static int fartParticleCount;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        enableFarts = getConfig().getBoolean("EnableFarts");
-        fartDistance = getConfig().getDouble("FartDistance");
-        fartTimeStart = getConfig().getDouble("FartTimeStart");
-        fartTimeEnd = getConfig().getDouble("FartTimeEnd");
-        fartOffset = getConfig().getDouble("FartOffset");
-        fartParticleCount = getConfig().getInt("FartParticleCount");
-        fartParticleSize = getConfig().getDouble("FartParticleSize");
-        fartVolume = getConfig().getDouble("FartVolume");
-        poopChance = getConfig().getDouble("PoopChance");
+        enableFarts = getConfig().getBoolean("EnableFarts", true);
+        fartDistance = getConfig().getDouble("FartDistance", 0.8);
+        fartTimeStart = getConfig().getDouble("FartTimeStart", 15);
+        fartTimeEnd = getConfig().getDouble("FartTimeEnd", 30);
+        fartOffset = getConfig().getDouble("FartOffset", 0.25);
+        fartParticleCount = getConfig().getInt("FartParticleCount", 25);
+        fartParticleSize = getConfig().getDouble("FartParticleSize", 1);
+        fartVolume = getConfig().getDouble("FartVolume", 1);
+        poopChance = getConfig().getDouble("PoopChance", 0.05);
+        nauseaChance = getConfig().getDouble("NauseaChance", 0.3);
+        nauseaDistance = getConfig().getDouble("NauseaDistance", 2);
 
-        if(enableFarts)
+        if (enableFarts)
             getServer().getPluginManager().registerEvents(new FartListener(), this);
 
     }
