ebf0e1e6b11f204fab20dc2f132b5f60818f6a51
nezd5553 <nezdatsdev@gmail.com>
2021-08-08 15:06:22 -0700

    Create FartCommandExecutor.java

---
+++ src/main/java/nezd53/sneakfart/FartCommandExecutor.java
@@ -0,0 +1,16 @@
+package nezd53.sneakfart;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FartCommandExecutor implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(commandSender instanceof Player)
+            FartHandler.fart((Player)commandSender);
+
+        return true;
+    }
+}
