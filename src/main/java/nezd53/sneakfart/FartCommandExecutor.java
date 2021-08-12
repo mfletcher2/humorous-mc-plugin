601481c863700ff470d5474833ef78c31a0bd084
nezd5553 <nezdatsdev@gmail.com>
2021-08-12 13:02:03 -0700

    Idea added a space so now I need to commit it

--- src/main/java/nezd53/sneakfart/FartCommandExecutor.java
+++ src/main/java/nezd53/sneakfart/FartCommandExecutor.java
@@ -8,8 +8,8 @@ import org.bukkit.entity.Player;
 public class FartCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if(commandSender instanceof Player)
-            FartHandler.fart((Player)commandSender);
+        if (commandSender instanceof Player)
+            FartHandler.fart((Player) commandSender);
 
         return true;
     }
